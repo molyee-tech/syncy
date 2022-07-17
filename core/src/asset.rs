14f9ba5c193b66d5cae01fad83cad3c2da38b9c3
Alex Sarapulov <1molyee@gmail.com>
2022-07-17 13:42:28 +0700

    move token

--- core/src/asset.rs
+++ core/src/asset.rs
@@ -5,3 +5,14 @@ pub struct Unique<H: Hash> {
     meta: Url,
     data: Url,
 }
+
+pub struct Token {
+    unique: Unique,
+    source: source::Id,
+    parent: Option<Id>,
+}
+
+pub struct Info {
+    name: String,
+    desc: String,
+}
--- core/src/token.rs
+++ core/src/token.rs
@@ -1,12 +1,1 @@
 pub struct Id(u128);
-
-pub struct Token {
-    unique: Unique,
-    source: source::Id,
-    parent: Option<Id>,
-}
-
-pub struct Info {
-    name: String,
-    desc: String,
-}
