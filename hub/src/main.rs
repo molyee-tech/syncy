16679bd0e2f1513104dc3ae15ac737ccab15ee3d
Alex Sarapulov <1molyee@gmail.com>
2022-08-17 17:09:58 +0700

    {hub} reimpl from cli

--- hub/src/main.rs
+++ hub/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use color_eyre::eyre::Result;
+use fluvio_cli::{Root, HelpOpt};
+use fluvio_future::task::run_block_on;
+
+fn main() -> Result<()> {
+    fluvio_future::subscriber::init_tracer(None);
+    color_eyre::config::HookBuilder::blank()
+        .display_env_section(false)
+        .install()?;
+    print_help_hack()?;
+    let root: Root = Root::parse();
+
+    // If the CLI comes back with an error, attempt to handle it
+    if let Err(e) = run_block_on(root.process()) {
+        e.print()?;
+        std::process::exit(1);
+    }
+
+    Ok(())
+}
+
+fn print_help_hack() -> Result<()> {
+    let mut args = std::env::args();
+    if args.len() < 2 {
+        HelpOpt {}.process()?;
+        std::process::exit(0);
+    } else if let Some(first_arg) = args.nth(1) {
+        if vec!["-h", "--help", "help"].contains(&first_arg.as_str()) {
+            HelpOpt {}.process()?;
+            std::process::exit(0);
+        }
+    }
+    Ok(())
 }
