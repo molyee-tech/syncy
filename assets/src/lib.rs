1e5494a6adaab305cda3a0c326f38201287359f2
Alex Sarapulov <1molyee@gmail.com>
2022-08-17 14:06:11 +0700

    {assets} move out from chain; add basic traits

---
+++ assets/src/lib.rs
@@ -0,0 +1,6 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate core;
+
+pub mod error;
+pub mod traits;
---
+++ assets/src/traits.rs
@@ -0,0 +1,23 @@
+use core::hash::Hasher;
+
+pub struct Hash<const N: usize>([u8; N]);
+
+trait Unique<H: Hasher, const N: usize> {
+    fn token(&self) -> Hash<N>;
+}
+
+trait Amount<Value> {
+    fn amount(&self) -> Value;
+}
+
+trait Meta {
+    fn meta(&self) -> &[u8];
+}
+
+trait Asset {
+    fn payload(&self) -> &[u8];
+}
+
+trait Owned<U> {
+    fn owner(&self) -> U;
+}
