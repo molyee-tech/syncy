d6e99c42c34507509cc147a3f7e0be26440851ba
Alex Sarapulov <1molyee@gmail.com>
2022-09-04 16:39:50 +0700

    {node} build transport

---
+++ node/src/transport.rs
@@ -0,0 +1,25 @@
+
+pub fn build_transport(
+    key_pair: identity::Keypair,
+    psk: Option<PreSharedKey>,
+) -> transport::Boxed<(PeerId, StreamMuxerBox)> {
+    let noise_keys = noise::Keypair::<noise::X25519Spec>::new()
+        .into_authentic(&key_pair)
+        .unwrap();
+    let noise_config = noise::NoiseConfig::xx(noise_keys).into_authenticated();
+    let yamux_config = YamuxConfig::default();
+
+    let base_transport = TcpTransport::new(GenTcpConfig::default().nodelay(true));
+    let maybe_encrypted = match psk {
+        Some(psk) => EitherTransport::Left(
+            base_transport.and_then(move |socket, _| PnetConfig::new(psk).handshake(socket)),
+        ),
+        None => EitherTransport::Right(base_transport),
+    };
+    maybe_encrypted
+        .upgrade(Version::V1)
+        .authenticate(noise_config)
+        .multiplex(yamux_config)
+        .timeout(Duration::from_secs(20))
+        .boxed()
+}
