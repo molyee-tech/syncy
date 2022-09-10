cb4fb76dd5b325ae96bb2fd5346dd76640daacc3
Alex Sarapulov <1molyee@gmail.com>
2022-09-10 17:44:32 +0700

    {node} listening

--- node/src/node.rs
+++ node/src/node.rs
@@ -1,4 +1,16 @@
+use log::*;
+
 pub struct Node<RT> {
     rt: RT,
     swarm: Swarm<Behaviour>,
+}
+
+impl<RT> Node<RT> {
+    pub fn listen<A: Iterator<Item = Multiaddr>(&mut self, addrs: A) -> Result<()> {
+        for addr in addrs {
+            info!("Start listening {:?} ..", addr);
+            Swarm::listen_on(&mut self.swarm, addr)?;
+        }
+        Ok(())
+    }
 }
